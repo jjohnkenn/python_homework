c28a9dd471dd98d03a44fd610a350276841f1c9e
jjohnkenn <jjohnkenn@gmail.com>
2020-05-06 19:22:05 +0530

    leart python print with sep

---
+++ learn_print_hw.py
@@ -0,0 +1,8 @@
+#normal print, end-'    '
+print("hi Joshua")
+#print with end
+print("hi Joshua")
+print("hi Joshua")
+#print with sep
+print("hi","I am Joshua","i am studying in 7th standard",sep=",")
+#print file name
